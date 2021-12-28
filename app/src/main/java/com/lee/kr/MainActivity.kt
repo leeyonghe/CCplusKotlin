1b0616cd5224c2c06c10c598c1f1e7b42649bcf0
leeyonghe <lyhdra99@gmail.com>
2021-12-28 19:17:25 +0900

    init

---
+++ app/src/main/java/com/lee/kr/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lee.kr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.lee.kr.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'kr' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'kr' library on application startup.
+        init {
+            System.loadLibrary("kr")
+        }
+    }
+}
